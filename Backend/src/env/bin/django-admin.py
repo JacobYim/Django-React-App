ebb7baacf75c7e0430a07697ce0d0f41f3ab92c1
JacobYim <dlagudah@gmail.com>
2019-07-13 15:15:58 +0900

    environment setting

---
+++ Backend/src/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/junghwanyim/Documents/GitHub/django-react-app/Django-React-App/Backend/src/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
