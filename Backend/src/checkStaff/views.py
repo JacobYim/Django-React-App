6029d23c39c953fe1984cebf608b5ab3bf8ea9a3
JacobYim <dlagudah@gmail.com>
2019-07-15 14:19:51 +0000

    add token access with rest-api

--- Backend/src/checkStaff/views.py
+++ Backend/src/checkStaff/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 import json
 from io import StringIO
 
@@ -8,14 +9,26 @@ from io import StringIO
 def hello_world(request) :
     return Response({"message":"Hello, World!"})
 
+# @api_view(['GET'])
+# def hello(request) :
+#     io = StringIO()
+#     content = User.objects.all()
+#     # str1 = User.objects.filter(last_name='')
+#     # str2 = ''.join(content.get(username = 'admin123').username) 
+#     # str2 = ''.join(content.get(username = 'admin123'))
+#     staff = content.filter(is_staff=True)
+#     str2 = staff.values_list('username')
+#     return Response({'result' : str2})
+#     # https://wayhome25.github.io/django/2017/04/01/django-ep9-crud/
+
 @api_view(['GET'])
 def hello(request) :
     io = StringIO()
-    content = User.objects.all()
+    content = Token.objects.all()
     # str1 = User.objects.filter(last_name='')
     # str2 = ''.join(content.get(username = 'admin123').username) 
     # str2 = ''.join(content.get(username = 'admin123'))
-    staff = content.filter(is_staff=True)
-    str2 = staff.values_list('username')
+    # staff = content.exclude(user='')
+    str2 = content.values_list('user')
     return Response({'result' : str2})
     # https://wayhome25.github.io/django/2017/04/01/django-ep9-crud/
